2766e5cba976578f74c44b5cf3dbea175045f909
ConMu <Con_Mu@163.com>
2022-04-20 21:10:07 +0800

    springboot study

--- springboot_11_log/src/main/java/com/conmu/controller/BookController.java
+++ springboot_11_log/src/main/java/com/conmu/controller/BookController.java
@@ -1,17 +1,30 @@
 package com.conmu.controller;
 
+import com.conmu.utils.BaseClass;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//lombok创建，日志方式三
+@Slf4j
 //Rest模式
 @RestController
 @RequestMapping("/books")
 
-public class BookController {
+public class BookController{
+    //创建记录日志的对象,方法一
+//    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     @GetMapping
     public String getById() {
         System.out.println("springboot is running...");
+        log.info("info...");
+        log.warn("warn...");
+        log.debug("debug...");
+        log.error("error...");
+
         return "springboot is running...";
     }
 }
--- springboot_11_log/src/main/java/com/conmu/utils/BaseClass.java
+++ springboot_11_log/src/main/java/com/conmu/utils/BaseClass.java
@@ -1,4 +1,15 @@
 package com.conmu.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BaseClass {
+
+    private final Class clazz;
+    //日志对象方法二,采用方法继承，log以静态变量存储
+    public static Logger log;
+    public BaseClass() {
+        clazz = this.getClass();
+        log = LoggerFactory.getLogger(clazz);
+    }
 }
