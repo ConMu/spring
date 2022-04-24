4b7f287931f5827ba7e27e154cfe41432b517c67
ConMu <Con_Mu@163.com>
2022-04-24 23:54:23 +0800

    springboot study

---
+++ springboot_16_redis/src/test/java/com/conmu/Springboot16RedisApplicationTests.java
@@ -0,0 +1,43 @@
+package com.conmu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class Springboot16RedisApplicationTests {
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	void set() {
+		ValueOperations ops = redisTemplate.opsForValue();
+		ops.set("age", 41);
+	}
+	@Test
+	void get() {
+		ValueOperations ops = redisTemplate.opsForValue();
+		Object age = ops.get("age");
+		System.out.println(age);
+	}
+
+	@Test
+	void hset() {
+		HashOperations ops = redisTemplate.opsForHash();
+		ops.put("info","a","aa1");
+		ops.put("info","b","aa2");
+	}
+	@Test
+	void hget() {
+		HashOperations ops = redisTemplate.opsForHash();
+		Object o1 = ops.get("info", "a");
+		Object o2 = ops.get("info", "b");
+		System.out.println(o1);
+		System.out.println(o2);
+	}
+
+
+}
