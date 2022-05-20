2cc90dadb5bc950b05e4fab66e1470b0fe5804dc
mucongcong <mucongcong@corp.netease.com>
2022-05-20 15:37:40 +0800

    [add] redis Config&Util 2022/05/20

--- springboot_16_redis/src/test/java/com/conmu/Springboot16RedisApplicationTests.java
+++ springboot_16_redis/src/test/java/com/conmu/Springboot16RedisApplicationTests.java
@@ -1,5 +1,7 @@
 package com.conmu;
 
+import com.conmu.po.Book;
+import com.conmu.util.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,16 +9,32 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class Springboot16RedisApplicationTests {
 	@Autowired
 	//以object 对象 为操作的基本单元，因此操作后在客户端不能查询出来
 	private RedisTemplate redisTemplate;
 
+	@Resource
+	private RedisUtil redisUtil;
+
+	private final String BOOK_PREFIX = "book";
 	@Test
 	void set() {
 		ValueOperations ops = redisTemplate.opsForValue();
 		ops.set("age", 41);
+	}
+
+	@Test
+	void setObjectByUtil() {
+		Book book = new Book();
+		book.setId(1);
+		book.setName("梦回紫金城");
+		book.setType("mcc");
+		book.setDescription("this is my dream");
+		redisUtil.set(BOOK_PREFIX, book);
 	}
 	@Test
 	void get() {
