4c31e0932e6b7ee1e797c74c27c70680fd69e9c7
mucongcong <mucongcong@corp.netease.com>
2022-05-20 15:37:40 +0800

    [add] redis Config&Util 2022/05/20

---
+++ springboot_16_redis/src/main/java/com/conmu/po/Book.java
@@ -0,0 +1,15 @@
+package com.conmu.po;
+
+import lombok.Data;
+
+//lombok
+//@Getter
+//@Setter
+@Data
+public class Book {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+
+}
